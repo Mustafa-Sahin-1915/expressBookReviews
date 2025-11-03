bb4ec95e7fdd894a55497a27fb75e1cb253b826d
vjavs <1909485+viniciusjavs@users.noreply.github.com>
2025-11-02 19:38:27 -0500

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,8 +62,16 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    try {
+      var book = books[isbn]
+      if (!book) {
+          throw Error("Book not found")
+      }
+      res.status(200).json(JSON.stringify(book.reviews))
+    } catch (error) {
+      return res.status(404).json({message: error.message});
+    }
 });
 
 module.exports.general = public_users;
