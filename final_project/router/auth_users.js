8b3d39f334e4660905d4a449db9c8d78b9b21bfa
vjavs <1909485+viniciusjavs@users.noreply.github.com>
2025-11-03 10:24:09 -0500

    Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,28 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    {username: "User", password: "Pass"}
+];
+
+const jwtSecret = "095e8657503e7fea0e4f90d6ca0e0b32"
 
 const isValid = (username)=>{ //returns boolean
     return !users.find(user => user.username === username)
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password)=> {
+    return users.some(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+  const token = jwt.sign({ username }, jwtSecret, { expiresIn: '1h' });
+  return res.status(200).json({ message: "Login successful", token });
 });
 
 // Add a book review
