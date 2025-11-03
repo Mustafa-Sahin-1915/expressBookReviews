6465326df9bd4cabd3ff54a1a50b8298a3524f6a
vjavs <1909485+viniciusjavs@users.noreply.github.com>
2025-11-03 13:50:12 -0500

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -41,6 +41,19 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   return res.status(200).json({ message: "Review added/updated successfully", reviews: books[isbn].reviews });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const { review } = req.body;
+    const username = req.user?.username;
+  
+    if (!books[isbn]) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  
+    delete books[isbn].reviews[username];
+    return res.status(200).json({ message: "Review deleted successfully", reviews: books[isbn].reviews });
+  });
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
