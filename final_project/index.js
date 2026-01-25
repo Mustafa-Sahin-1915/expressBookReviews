ab6332cd862655c37f1b9028dee69f861b17b2c8
Temicodes2 <temidayosodeinde360@gmail.com>
2026-01-24 20:43:21 -0500

    bang

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,25 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Check if the session has an authorization object
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        // Verify the JWT token
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                // If token is valid, attach user to request and proceed
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        // If no token is found in the session
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
