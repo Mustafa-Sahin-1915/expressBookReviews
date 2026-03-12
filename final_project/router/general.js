7a3ceb547ebe16bf0a6240f02e81f4b144850913
Mustafa Sahin <100782317+Mustafa-Sahin-1915@users.noreply.github.com>
2026-03-12 22:18:27 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,71 +26,115 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    new Promise((resolve, reject) => {
-        resolve(books);
-    }).then((bookList) => {
+public_users.get('/', async (req, res)=> {
+    try {
+        // Promise'i await ile çözüyoruz
+        const bookList = await new Promise((resolve, reject) => {
+            if (books) {
+                resolve(books);
+            } else {
+                reject("Books not found");
+            }
+        });
+
+        // Başarılı ise veriyi dön
         res.status(200).json(bookList);
-    }).catch((err) => {
-        res.status(500).json({ message: "Error" });
-    });
+
+    } catch (err) {
+        // Hata varsa mesajla birlikte döndür
+        res.status(500).json({ message: "Error: " + err });
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    new Promise((resolve, reject) => {
-        const book = books[req.params.isbn];
-        if (book) {
-            resolve(book);
-        } else {
-            reject("Book not found");
-        }
-    }).then((book) => {
+public_users.get('/isbn/:isbn',async (req, res)=> {
+    try {
+        // Promise'i await ile doğrudan çözüyoruz
+        const book = await new Promise((resolve, reject) => {
+            const bookData = books[req.params.isbn];
+            if (bookData) {
+                resolve(bookData);
+            } else {
+                reject("Book not found");
+            }
+        });
+
+        // Eğer Promise başarılıysa (resolve), kod buraya devam eder
         res.status(200).json(book);
-    }).catch((err) => {
+
+    } catch (err) {
+        // Eğer Promise hata dönerse (reject), kod buraya düşer
         res.status(404).json({ message: err });
-    });
+    }
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res){
-    new Promise((resolve, reject) => {
-        const author = req.params.author;
-        const booksByAuthor = Object.values(books).filter(b => b.author === author);
-        if (booksByAuthor.length > 0) resolve(booksByAuthor);
-        else reject("No books found for this author");
-    }).then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
+public_users.get('/author/:author',async (req, res)=>{
+    try {
+        const booksByAuthor = await new Promise((resolve, reject) => {
+            const author = req.params.author;
+            const filteredBooks = Object.values(books).filter(b => b.author === author);
+            
+            if (filteredBooks.length > 0) {
+                resolve(filteredBooks);
+            } else {
+                reject("No books found for this author");
+            }
+        });
+
+        // Promise başarıyla sonuçlanırsa veriyi dön
+        res.status(200).json(booksByAuthor);
+
+    } catch (err) {
+        // Promise hata dönerse (reject) buraya düşer
         res.status(404).json({ message: err });
-    });
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    new Promise((resolve, reject) => {
-        const title = req.params.title;
-        const booksByTitle = Object.values(books).filter(b => b.title === title);
-        if (booksByTitle.length > 0) resolve(booksByTitle);
-        else reject("No books found for this title");
-    }).then((result) => {
-        res.status(200).json(result);
-    }).catch((err) => {
+public_users.get('/title/:title',async (req, res)=> {
+    try {
+        // Promise'i await ile çözüyoruz
+        const booksByTitle = await new Promise((resolve, reject) => {
+            const title = req.params.title;
+            const filteredBooks = Object.values(books).filter(b => b.title === title);
+            
+            if (filteredBooks.length > 0) {
+                resolve(filteredBooks);
+            } else {
+                reject("No books found for this title");
+            }
+        });
+
+        // Başarılıysa veriyi JSON olarak dön
+        res.status(200).json(booksByTitle);
+
+    } catch (err) {
+        // Hata durumunda 404 dön
         res.status(404).json({ message: err });
-    });
+    }
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-    new Promise((resolve, reject) => {
-        const book = books[req.params.isbn];
-        if (book) resolve(book.reviews);
-        else reject("Book not found");
-    }).then((reviews) => {
+public_users.get('/review/:isbn',async (req, res)=> {
+    try {
+        // Promise'i await ile bekletiyoruz
+        const reviews = await new Promise((resolve, reject) => {
+            const book = books[req.params.isbn];
+            if (book) {
+                resolve(book.reviews);
+            } else {
+                reject("Book not found");
+            }
+        });
+
+        // Başarılı ise yorumları döndür
         res.status(200).json(reviews);
-    }).catch((err) => {
+
+    } catch (err) {
+        // Hata durumunda 404 dön
         res.status(404).json({ message: err });
-    });
+    }
 });
 
 module.exports.general = public_users;
